f283c9010df8c837c872de8724b43959b323838e
JP Talusan <jptalusan@gmail.com>
2025-07-10 11:39:59 -0500

    Added harpoon delete i<C-d>, moved lsp changes to separate file.

--- init.lua
+++ init.lua
@@ -118,14 +118,6 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "c", "cpp", "h", "hpp" },
-	callback = function()
-		vim.bo.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
-		vim.bo.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
-		vim.bo.expandtab = true -- Use spaces instead of tabs, set to false if you prefer real tabs
-	end,
-})
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
---
+++ lua/jose/lsp.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "cpp", "h", "hpp" },
+	callback = function()
+		vim.bo.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+		vim.bo.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+		vim.bo.expandtab = true -- Use spaces instead of tabs, set to false if you prefer real tabs
+	end,
+})
